95041fb79bf7a07fc80b740ca5e8de0be07d95d3
wangyi <wangyi@lanqb.cn>
2020-03-26 21:08:47 +0800

    fix

--- crawler/spiders/douban.py
+++ crawler/spiders/douban.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from crawler.items import CrawlerItem
 
 class DoubanSpider(scrapy.Spider):
     name = 'douban'
-    allowed_domains = ['https://www.douban.com/']
+    allowed_domains = ['www.douban.com']
     start_urls = ['https://www.douban.com/group/HZhome/']
 
     def parse(self, response):
-          filename = response.url.split("/")[-2] # 获取url，用”/”分段，获去倒数第二个字段
-          with open(filename, 'a') as f:
-                f.write(response.body) # 把访问的得到的网页源码写入文件
-        # filename
-        # pass
+        dl = response.css('.mod table')
+        # filename = response.url.split("/")[-2]  # 获取url，用”/”分段，获去倒数第二个字段
+        # with open(filename, 'ab') as f:
+            # f.write(response.body)  # 把访问的得到的网页源码写入文件
+        for dd in dl:
+            item = CrawlerItem()
+            item['title'] = dd.css('.title a::text').extract_first()
+            # item['nowrap'] = dd.css('.nowrap a::text').extract_first()
+            yield item
+
+        #   filename = response.url.split("/")[-2] # 获取url，用”/”分段，获去倒数第二个字段
+        #   with open(filename, 'ab') as f:
+            #   f.write(response.body)  # 把访问的得到的网页源码写入文件
